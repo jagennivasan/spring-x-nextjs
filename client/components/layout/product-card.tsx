e29a65162c37c1efba9d51d7057b82f1a913f6af
jagennivasan <professorjagan18@gmail.com>
2025-04-15 13:14:00 +0530

    adding image to the product card

--- client/components/layout/product-card.tsx
+++ client/components/layout/product-card.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 interface ProductShape {
   id: string;
   name: string;
@@ -14,6 +16,16 @@ export default function ProductCard({ product }: { product: ProductShape }) {
   return (
     <div className="flex flex-col w-fit p-5 ">
       <div className="bg-gray-600 space-y-5   p-5 rounded-2xl shadow ">
+        {product.id ? (
+          <Image
+            src={`http://localhost:8080/api/products/${product.id}/image`}
+            alt="product image"
+            width={200}
+            height={200}
+          />
+        ) : (
+          <p>Image not available</p>
+        )}
         <h2 className="text-xl">{product.name}</h2>
         <p className="">{product.description}</p>
         <p>{product.brand}</p>
